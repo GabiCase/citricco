49216a935fbad8c8af980cc3adf4db6a3f1f4c9f
anabermudezmonteagudo <anabermudezmonteagudo@gmail.com>
2020-10-15 10:13:58 +0200

    cambios en cart

--- client/src/components/pages/index/TrendCarrousel.js
+++ client/src/components/pages/index/TrendCarrousel.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./TrendCarrousel.css";
 import Slider from "react-slick";
 
+import { Link } from 'react-router-dom'
+
 export default class TrendCarrousel extends Component {
   render() {
     var settings = {
@@ -43,34 +45,50 @@ export default class TrendCarrousel extends Component {
         <h4> Trending now </h4>
         <Slider {...settings}>
           <div>
-            <img
-              src="https://res.cloudinary.com/def9y4z86/image/upload/v1602589778/citricco/figleaf2_ijwi8v.jpg"
-              alt="fig-leaf earring"
-            />
+            <Link to="/products/details/5f8594305f9d33184cdb6504">
+              <img
+                src="https://res.cloudinary.com/def9y4z86/image/upload/v1602589778/citricco/figleaf2_ijwi8v.jpg"
+                alt="fig-leaf earring"
+              />
+            </Link>
           </div>
           <div>
-            <img
-              src="https://res.cloudinary.com/def9y4z86/image/upload/v1602595573/citricco/vera5_veddgl.jpg"
-              alt="vera earring"
-            />
+            <Link to="/products/details/5f85aac90467fd4a28702baf">
+              <img
+                src="https://res.cloudinary.com/def9y4z86/image/upload/v1602595573/citricco/vera5_veddgl.jpg"
+                alt="vera earring"
+              />
+            </Link>
           </div>
           <div>
-            <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602595012/citricco/debaja.jpg.jpg" />
+            <Link to="/products/details/5f85a8db5f9d33184cdb6508">
+              <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602595012/citricco/debaja.jpg.jpg" alt="be baja earring" />
+            </Link>
           </div>
           <div>
-            <h3>4</h3>
+            <Link to="/products/details/5f85ac0b0467fd4a28702bb0">
+              <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602595934/citricco/medusa4_ayibil.jpg" alt="medua earring" />
+            </Link>
           </div>
           <div>
-            <h3>5</h3>
+            <Link to="/products/details/5f8595de5f9d33184cdb6506">
+              <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602590376/citricco/clea2_beymiz.jpg" alt=" clea earring" />
+            </Link>
           </div>
           <div>
-            <h3>6</h3>
+            <Link to="/products/details/5f85931f5f9d33184cdb6503">
+              <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602589504/citricco/primas6_ntvyls.jpg" alt="prisma earring" />
+            </Link>
           </div>
           <div>
-            <h3>7</h3>
+            <Link to="/products/details/5f8595265f9d33184cdb6505">
+              <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602590000/citricco/petal3_cl9uor.jpg" alt="petal earring" />
+            </Link>
           </div>
           <div>
-            <h3>8</h3>
+            <Link to="/products/details/5f8591ad5f9d33184cdb6502">
+              <img src="https://res.cloudinary.com/def9y4z86/image/upload/v1602588841/citricco/celaya3_xmyrmu.jpg" alt="celaya earring" />
+            </Link>
           </div>
         </Slider>
       </div>
